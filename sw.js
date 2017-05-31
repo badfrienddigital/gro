f5ef932501348c3c21d66244d18bf3d05bbecfea
badfrienddigital <badfrienddigital@gmail.com>
2017-05-31 02:25:42 -0400

    sw update

--- sw.js
+++ sw.js
@@ -1,29 +1,29 @@
 this.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        'audio/organ-low-2m-48-b.mp4',
-        'audio/organ-med-8m-48.mp4',
-        'audio/organ-hi-7m-48.mp4',
-        'audio/organ-hi-er-9m-48.mp4',
-        'audio/organ-hi-est-6m-48.mp4',
-        'audio/organ-low-2m-b.mp4',
-        'audio/organ-med-8m.mp4',
-        'audio/organ-hi-7m.mp4',
-        'audio/organ-hi-er-9m.mp4',
-        'audio/organ-hi-est-6m.mp4',
-      ]);
-    })
-  );
+  // event.waitUntil(
+  //   caches.open('v1').then(function(cache) {
+  //     return cache.addAll([
+  //       'audio/organ-low-2m-48-b.mp4',
+  //       'audio/organ-med-8m-48.mp4',
+  //       'audio/organ-hi-7m-48.mp4',
+  //       'audio/organ-hi-er-9m-48.mp4',
+  //       'audio/organ-hi-est-6m-48.mp4',
+  //       'audio/organ-low-2m-b.mp4',
+  //       'audio/organ-med-8m.mp4',
+  //       'audio/organ-hi-7m.mp4',
+  //       'audio/organ-hi-er-9m.mp4',
+  //       'audio/organ-hi-est-6m.mp4',
+  //     ]);
+  //   })
+  // );
 });
 
-this.addEventListener('fetch', function(event) {
-  event.respondWith(caches.match(event.request).catch(function() {
-    return fetch(event.request);
-  }).then(function(response) {
-    caches.open('v1').then(function(cache) {
-      cache.put(event.request, response);
-    });
-    return response.clone();
-  }));
-});
+// this.addEventListener('fetch', function(event) {
+//   event.respondWith(caches.match(event.request).catch(function() {
+//     return fetch(event.request);
+//   }).then(function(response) {
+//     caches.open('v1').then(function(cache) {
+//       cache.put(event.request, response);
+//     });
+//     return response.clone();
+//   }));
+// });
